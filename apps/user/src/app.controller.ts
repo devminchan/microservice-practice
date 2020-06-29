293fd1a771f76d2eb63025d5b0ef9796c7b52704
devminchan <minchan.dev@gmail.com>
2020-06-29 16:45:25 +0900

    Redis broker 메커니즘 확인

--- apps/user/src/app.controller.ts
+++ apps/user/src/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
+  private logger = new Logger('AppController');
+
   constructor(private readonly appService: AppService) {}
 
   @MessagePattern('getHello')
   getHello(name: string): string {
+    require('dns').lookup(require('os').hostname(), (err, add, fam) => {
+      this.logger.log(`received addr: ${add}`);
+    });
+
     return this.appService.getHello(name);
   }
 }
